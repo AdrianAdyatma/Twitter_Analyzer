dd4513933f68d2c0be10f0941ce82cf33b0f2232
Adrian-Ryzen <adrianadyatma21@gmail.com>
2019-04-21 16:37:54 +0700

    Test fix vulnerability issue with urllib3

--- experimental/multithreading.py
+++ experimental/multithreading.py
@@ -1,30 +1,23 @@
 import time
 import threading
-import multiprocessing
 
 
-def calc_square(number):
-    print('Square:', number * number)
-
-
-def calc_quad(number):
-    print('Quad:', number * number * number * number)
+def Threading(arg):
+    while True:
+        time.sleep(1)
+        print('THREAD', arg)
 
 
 if __name__ == "__main__":
     t = time.time()
 
-    number = 73
-    thread1 = threading.Thread(target=calc_square, args=(number,))
-    thread2 = threading.Thread(target=calc_quad, args=(number,))
-    # Will execute both in parallel
+    thread1 = threading.Thread(target=Threading, args=(1,))
     thread1.start()
+
+    thread2 = threading.Thread(target=Threading, args=(2,))
     thread2.start()
-    # Joins threads back to the parent process
+
     thread1.join()
     thread2.join()
-
-    # calc_square(number)
-    # calc_quad(number)
 
     print("Took", time.time() - t)
